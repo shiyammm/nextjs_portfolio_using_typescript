37100cc3c711c7d6466aeec84258ec3ac17c792f
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-02-29 22:57:00 +0530

    Add Navbar and Hero sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import Navbar from '@/containers/Navbar';
 import React from 'react';
+import Hero from '@/containers/Hero';
+import About from '@/containers/About';
 
 const page = () => {
-  return <div>Portfolio</div>;
+  return (
+    <main className=" w-full h-screen">
+      <div className="px-16 py-10 bg-gray">
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+    </main>
+  );
 };
 
 export default page;

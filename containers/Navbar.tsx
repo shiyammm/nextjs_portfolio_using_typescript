d019bbad62d01a28606d037e8de8ce8c8aa78144
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-02-29 23:03:44 +0530

    Update Navar with toggle menu component

--- containers/Navbar.tsx
+++ containers/Navbar.tsx
@@ -1,7 +1,16 @@
+'use client';
 import React from 'react';
+import Menu from '@/components/Menu';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex justify-between items-center">
+      <header className="font-roslindale-display text-3xl text-white">
+        Shiyam
+      </header>
+      <Menu />
+    </div>
+  );
 };
 
 export default Navbar;

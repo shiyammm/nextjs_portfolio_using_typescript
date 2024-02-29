1ccbec1a33418b49122e57d7f36e1a644dc62114
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-02-29 23:02:57 +0530

    Update Hero section with blob and h1

--- containers/Hero.tsx
+++ containers/Hero.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Blob from '@/components/Blob';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="w-full h-screen flex justify-center relative z-10">
+      <Blob />
+      <div className="mt-[14rem]">
+        <h1 className="leading-[11rem] font-roslindale-display text-[11rem] text-white">
+          Let's innovate <br />
+          <span className="flex items-center gap-8 justify-center h-full w-full">
+            <span className="inline-block font-acme text-2xl mt-6 font-semibold tracking-wide bg-cyan px-6 py-4 rounded-full text-black">
+              digital experiences
+            </span>{' '}
+            together,
+          </span>
+        </h1>
+        <span className="text-[2rem] font-circular-book flex justify-center mt-10 text-white ">
+          crafting them with code
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;

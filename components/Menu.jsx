d5108bae71a30df281cc21552424e11defc7c8b5
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-02-29 23:01:07 +0530

    Update menu component with toggle function

---
+++ components/Menu.jsx
@@ -0,0 +1,64 @@
+'use client';
+import React from 'react';
+import { useState } from 'react';
+
+const Menu = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleMenu = () => {
+    setToggle(!toggle);
+  };
+  return (
+    <>
+      <div
+        onClick={handleMenu}
+        className="flex items-center gap-4 absolute z-40 right-12 bg-violet px-5 py-2 text-black font-semibold active:border-none  rounded-full text-2xl"
+      >
+        <span className="mb-1.5">Menu</span>
+        <svg width="40" height="40" viewBox="0 0 100 100">
+          <path
+            className={`line line1 ${toggle && 'opened-line1'}`}
+            d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
+          />
+          <path
+            className={`line line2 ${toggle && 'opened-line2'}`}
+            d="M 20,50 H 80"
+          />
+          <path
+            className={`line line3 ${toggle && 'opened-line3'}`}
+            d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
+          />
+        </svg>
+      </div>
+      {toggle && (
+        <div className="absolute w-full h-screen left-0 top-0 flex items-center justify-center"  >
+          <ul className=" font-roslindale-display text-[11rem] text-white text-center leading-[15rem]">
+            <li>Work</li>
+            <li>Contact</li>
+            <li>CV</li>
+          </ul>
+          <div className="flex items-center justify-between w-full px-16 absolute bottom-8 text-white text-xl font-circular-medium ">
+            <a
+              href=""
+              className="group text-white transition-all duration-300 ease-in-out"
+            >
+              <span className="bg-left-bottom bg-gradient-to-r from-white to-white bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
+                LinkedIn
+              </span>
+            </a>
+            <a
+              href=""
+              className="group text-white transition-all duration-300 ease-in-out"
+            >
+              <span className="bg-left-bottom bg-gradient-to-r from-white to-white bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
+                Github
+              </span>
+            </a>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Menu;
